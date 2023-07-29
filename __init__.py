0de4e150aa60ff3ee13ceffda55d9db8d734ff6a
Topher.Shortt <shortt.c@gmail.com>
2023-07-28 21:08:49 -0700

    Initial

---
+++ __init__.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, url_for, request, redirect 
+
+app = Flask(__name__)
+
+#Home Page
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+#About Page
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+#Wordle Page
+@app.route("/wordle")
+def wordle():
+    return render_template("wordle.html")
+
+#Python LeetCode Page
+@app.route("/pythonleetcode")
+def pythonleetcode():
+    return render_template("pythonleetcode.html")
+
+#Java LeetCode Page
+@app.route("/javaleetcode")
+def javaleetcode():
+    return render_template("javaleetcode.html")
+
+#JavaScript LeetCode Page
+@app.route("/javascriptleetcode")
+def javascriptleetcode():
+    return render_template("javascriptleetcode.html")
+
+#Mini-Projects Page
+@app.route("/miniprojects")
+def miniprojects():
+    return render_template("miniprojects.html")
+
+#Contact Page
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+
+
+##### External Links #####
+
+#Routs to YouTube Channel
+@app.route("/youtube")
+def youtube():
+    return redirect("https://www.youtube.com/@thelonelydash")
+
+#Routs to Facebook Page
+@app.route("/facebook")
+def facebook():
+    return redirect("https://www.facebook.com/profile.php?id=100084781395705")
+
+#Routs to github page
+@app.route("/github")
+def github():
+    return redirect("https://github.com/TheLonelyDash")
+
+
