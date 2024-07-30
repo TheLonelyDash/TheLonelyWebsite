4c8fec0c3a19daad6898dcff4983a2c5427d089d
Chris <shortt.c@gmail.com>
2024-07-30 18:00:18 -0400

    embedded JS into the html of intervals

---
+++ static/intervals/intervalsjs.js
@@ -0,0 +1,16 @@
+
+const T1 = new Audio('static/audio/tone1.mp3')
+const T2 = new Audio('static/audio/tone2.mp3')
+
+function playT1() {
+    T1.play();
+};
+
+function playTt() {
+    T1.play();
+    T2.play();
+};
+
+function playT2() {
+    T2.play();
+};
